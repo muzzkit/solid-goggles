627f1845bb8d17e5c5d31e0aa68ad7a5462ecc1d
muiz azeez <66578279+muzzkit@users.noreply.github.com>
2023-09-05 17:19:09 +0100

    Update request-message.ts

--- pages/api/auth/request-message.ts
+++ pages/api/auth/request-message.ts
@@ -2,7 +2,7 @@ import Moralis from 'moralis';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const config = {
-  domain: process.env.APP_DOMAIN || 'CoinTR',
+  domain: process.env.NEXTAUTH_URL || 'CoinTR',
   statement: 'Please sign this message to confirm your identity.',
   uri: process.env.NEXTAUTH_URL || 'http://localhost:3000',
   timeout: 60,
